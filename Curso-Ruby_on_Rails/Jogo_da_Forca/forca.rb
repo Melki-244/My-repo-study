72a8e8e0760b656ca67ffb262dcae784cc99f1dd
Melki-244 <melkicavalcante244@gmail.com>
2021-04-22 13:22:57 -0300

    Mod 1

--- Curso-Ruby_on_Rails/Jogo_da_Forca/forca.rb
+++ Curso-Ruby_on_Rails/Jogo_da_Forca/forca.rb
@@ -1,29 +1,14 @@
-def da_boas_vindas
-  puts "Bem vindo ao joga da forca"
-  puts "qual o seu nome"
-  nome = gets.strip
-  puts "começaremos o jogo para você #{nome}."
-  nome
-end
-def escolhe_palavra_secreta
-  puts "escolhendo palavra secreta ..."
-  palavra_secreta = "pimba"
-  puts "palavra secreta com #{palavra_secreta.size} letras escolhidas."
-  palavra_secreta
-end
-def nao_quer_jogar?
-  puts "deseja jogar novamente (S/N)"
-  quero_jogar = gets.strip
-  nao_quero_jogar = quero_jogar.upcase == "N"
-end
-def pede_um_chute (erros,chutes)
-  puts "\n\n\n" 
-  puts "erros até agora: #{erros}"
-  puts "chutes até agora: #{chutes}"
-  puts "entre com uma letra ou uma palavra."
-  chute = gets.strip 
-  puts "será que acertou você chutou #{chute}"
-  chute
+require_relative 'ui'
+def pede_um_chute_valido (chutes, erros)
+    cabecalho_de_tentativa chutes, erros
+  loop do
+    chute = pede_um_chute 
+    if chutes.include? chute 
+        avisa_chute_repetido chute
+    else
+    return chute
+    end
+  end
 end
 def joga (nome)
   palavra_secreta = escolhe_palavra_secreta
@@ -31,40 +16,39 @@ def joga (nome)
   chutes = []
   pontos_ate_agora = 0
   while erros < 5
-    chute = pede_um_chute erros,chutes
+    chute = pede_um_chute_valido chutes, erros
     chutes << chute
     chute_single = chute.size == 1 
         if chute_single 
           letra_procurada = chute[0]
           total_encontrado = palavra_secreta.count  letra_procurada
           if total_encontrado == 0 
-            puts " Letra Não encontrada."
-            erros += 1 
+             avisa_letra_nao_encontrada
+             erros += 1
           else
-            puts "Letra Encontrada #{total_encontrado} vezes."
+            avisa_letra_encontrada total_encontrado
           end
         else
       acertou = chute == palavra_secreta
-      if acertou
-         puts "Parabéns Você acertou!"
+       if acertou
+         avisa_acerto
          pontos_ate_agora += 100
          break
        else
-          puts "Que pena... Você Errou"
           pontos_ate_agora -= 30
+          avisa_erro
           erros += 1 
        end
     end
   end
-  puts "você ganhou #{pontos_ate_agora} pontos."
+  avisa_pontos pontos_ate_agora
 end
-nome = da_boas_vindas
-loop do 
-  joga nome
-  if nao_quer_jogar?
-    break
+def jogo_da_forca
+  nome = da_boas_vindas
+  loop do 
+   joga nome
+   break if nao_quer_jogar?
   end
 end
-pimba do burro
 
 
---
+++ Curso-Ruby_on_Rails/Jogo_da_Forca/ui.rb
@@ -0,0 +1,61 @@
+def da_boas_vindas
+  puts "Bem vindo ao joga da forca"
+  puts "qual o seu nome"
+  nome = gets.strip
+  puts "começaremos o jogo para você #{nome}."
+  nome
+end
+def escolhe_palavra_secreta
+  puts "escolhendo palavra secreta ..."
+  palavra_secreta = "pimba"
+  puts "palavra secreta com #{palavra_secreta.size} letras escolhidas."
+  palavra_secreta
+end
+def nao_quer_jogar?
+  puts "deseja jogar novamente (S/N)"
+  quero_jogar = gets.strip
+  nao_quero_jogar = quero_jogar.upcase == "N"
+end
+def cabecalho_de_tentativa(chutes, erros)
+    puts "Palavra secreta:"
+    puts "\n\n\n\n"
+    puts "Erros até agora: #{erros}"
+    puts "Chutes até agora: #{chutes}"
+end
+def pede_um_chute(chutes, erros)
+    puts "Entre com a letra ou palavra"
+    chute = gets.strip
+    puts "Será que acertou? Você chutou #{chute}"
+    chute
+end
+def nao_quer_jogar?
+  puts "deseja jogar novamente (S/N)"
+  quero_jogar = gets.strip
+  nao_quero_jogar = quero_jogar.upcase == "N"
+end
+def pede_um_chute
+  puts "entre com uma letra ou uma palavra."
+  chute = gets.strip 
+  puts "será que acertou você chutou #{chute}"
+  chute
+end
+
+def avisa_chute_repetido (chute)
+  puts "Você já Chutou #{chute}"
+end
+def avisa_letra_nao_encontrada
+   puts "Letra não encontrada!"
+end
+def avisa_letra_encontrada(total_encontrado)
+  puts"Letra encontrada #{total_encontrado} vezes"
+end
+def avisa_acerto
+ puts "Parabéns! Acertou!"
+end
+def avisa_erro
+  puts "que pena.... errou!"
+end
+def avisa_pontos (pontos_ate_agora)
+  puts "você ganhou #{pontos_ate_agora} pontos"
+end
+
